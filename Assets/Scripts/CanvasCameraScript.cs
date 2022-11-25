84509b78a96d52392ef6b47cb4a551192c55e4f5
Timothy Axel W <timothyaxelw@gmail.com>
2022-11-25 23:24:33 +0700

    Camera Done
    cuma camera akhir

---
+++ Assets/Scripts/CanvasCameraScript.cs
@@ -0,0 +1,70 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+public class CanvasCameraScript : MonoBehaviour
+{
+    public Camera static_camera;
+    public Camera walk_camera;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        static_camera.gameObject.SetActive(true);
+        walk_camera.gameObject.SetActive(false);
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    public void walkCamera()
+    {
+        static_camera.gameObject.SetActive(false);
+        walk_camera.gameObject.SetActive(true);
+    }
+
+    public void staticCamera()
+    {
+        static_camera.gameObject.SetActive(true);
+        walk_camera.gameObject.SetActive(false);
+    }
+
+    public void SceneA()
+    {
+        SceneManager.LoadScene("Garage_A_SI");
+    }
+
+    public void SceneB()
+    {
+        SceneManager.LoadScene("Garage_B_SI");
+    }
+
+    public void SceneC()
+    {
+        SceneManager.LoadScene("Garage_C_SI");
+    }
+
+    public void SceneD()
+    {
+        SceneManager.LoadScene("Garage_D_SI");
+    }
+
+    public void SceneE()
+    {
+        SceneManager.LoadScene("Garage_E");
+    }
+
+    public void SceneF()
+    {
+        SceneManager.LoadScene("Garage_F");
+    }
+
+    public void SceneG()
+    {
+        SceneManager.LoadScene("Garage_G");
+    }
+}
